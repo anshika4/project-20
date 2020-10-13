01907675327e90d08ab63ff2b0b96febb87ad9e1
anshika4 <72254585+anshika4@users.noreply.github.com>
2020-10-13 18:25:08 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,6 +1,5 @@
 var zenia;
-var tourus;
-var Cyclap;
+
 var wall;
 
 var speed,weight;
@@ -13,16 +12,12 @@ function setup() {
   
   
   
-  Cyclap=createSprite(50, 100, 50, 50);
-  Cyclap.shapeColor=("red");
-
-  tourus=createSprite(50, 300, 50, 50);
-  tourus.shapeColor=("red");
+  
 
   zenia=createSprite(50, 200, 50, 50);
   zenia.shapeColor=("red");
 
-  wall=createSprite(900, 100, 60, 900);
+  wall=createSprite(1200, 200, 60, height/2);
   wall.shapeColor=color(80,80,80);
   drawSprites();
 }
@@ -31,8 +26,7 @@ function draw() {
   background(0);  
 
   zenia.velocityX=speed;
-  tourus.velocityX=speed;
-  Cyclap.velocityX=speed;
+ 
 
   if(wall.x-zenia.x<(zenia.width+wall.width)/2  ){
     zenia.velocityX=0;
@@ -48,33 +42,8 @@ function draw() {
     }
   }
 
-if(wall.x-tourus.x<(tourus.width+wall.width)/2  ){
-    tourus.velocityX=0;
-    var deformation=0.5*weight*speed*speed/22509;
-    if(deformation>100){
-      tourus.shapeColor=color(255,0,0); 
-    }
-    if(deformation<180 && deformation>100){
-      tourus.shapeColor=color(230,230,0);
-    }
-    if(deformation<100){
-      tourus.shapeColor=color(0,255,0);
-    }
-  }
 
-  if(wall.x-Cyclap.x<(Cyclap.width+wall.width)/2  ){
-    Cyclap.velocityX=0;
-    var deformation=0.5*weight*speed*speed/22509;
-    if(deformation>100){
-      Cyclap.shapeColor=color(255,0,0); 
-    }
-    if(deformation<180 && deformation>100){
-      Cyclap.shapeColor=color(230,230,0);
-    }
-    if(deformation<100){
-      Cyclap.shapeColor=color(0,255,0);
-    }
-  }
+  
 
 
 
